019a44809d9737a8673168942a5d907933bef21e
Paul Sinnett <paul.sinnett@gmail.com>
2020-11-10 20:40:32 +0000

    test level

---
+++ Assets/Scripts/PhysicsGlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsGlider : MonoBehaviour
+{
+    Rigidbody physics;
+
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float velocity = physics.velocity.magnitude;
+    }
+}
